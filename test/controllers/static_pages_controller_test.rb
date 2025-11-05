5cf445cbc9bc385c967f7333f7ff73b4fe6c597a
deandoff <deandoff@gmail.com>
2025-11-05 12:32:15 +0300

    lab5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,24 +2,26 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get "/"
     assert_response :success
+    assert_select "title", "Akhmetgaleev Islam"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get "/help"
     assert_response :success
+    assert_select "title", "Akhmetgaleev Islam"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get "/about"
     assert_response :success
+    assert_select "title", "Akhmetgaleev Islam"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get "/contact"
     assert_response :success
-    assert_select "title", "Akhmetgaleev Islam" 
+    assert_select "title", "Akhmetgaleev Islam"
   end
-
 end
